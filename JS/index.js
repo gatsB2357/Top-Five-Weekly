15f6d31f9f59bc684df08a58b12a7369a3358f1d
Angel Acosta <acostaangel031@gmail.com>
2016-03-01 23:59:17 +0000

    Fixed Menu Animation

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,25 @@
 /*global $ */
 $(document).ready(function () {
-    'use strict';
-    $("#menu").click(function () {
+    $(".menu_img").click(function () {
         $("#genres_holder").animate({
             left: "0px"
-        }, 200);
+        }, 250);
         $("#body").animate({
-            left: "-250px"
-        }, 200);
+            left: "200px"
+        }, 250);
+        $(".menu_img").fadeOut(200);
+        $(".menu_img").css("display", "none");
+        $(".menu_img_close").fadeIn(50);
+    });
+    $(".menu_img_close").click(function() {
+        $(".menu_img_close").fadeOut(200);
+        $(".menu_img_close").css("display", "none");
+        $(".menu_img").fadeIn(50);
+        $("#genres_holder").animate({
+           left: "-200px" 
+        }, 250);
+        $("#body").animate({
+            left: "0px"
+        }, 250);
     });
 });
